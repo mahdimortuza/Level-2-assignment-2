c237879be2d1c329bd208adcf5a2008ed3eaa8c7
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-11-23 01:24:08 +0600

    middleware added

--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// import cors from "cors";
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+//parsers
+app.use(express_1.default.json());
+// app.use(cors());
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello world!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.json({ message: "received successfully" });
 });
 exports.default = app;
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+// import cors from "cors";
+import express, { Request, Response } from "express";
+const app = express();
+
+const port = 3000;
+
+//parsers
+app.use(express.json());
+// app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({ message: "received successfully" });
+});
+
+export default app;
