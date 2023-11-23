b18fed7a0560cad65f08fb34eae6e8ee6e816531
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-11-23 10:33:38 +0600

    routes created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 
 const port = 3000;
@@ -8,13 +8,48 @@ const port = 3000;
 app.use(express.json());
 // app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
+const userRouter = express.Router();
+
+// retrieve a list of all users
+app.get(
+  "/api/users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send(something);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    res.status(400).json({
+      success: false,
+      message: "something went wrong",
+    });
+  }
 });
 
-app.post("/", (req: Request, res: Response) => {
+// not found route
+// app.all("*", (req: Request, res: Response) => {
+//   res.status(400).json({
+//     success: false,
+//     message: "route not found",
+//   });
+// });
+
+// retrieve a specific user by ID
+app.get("/api/users/:userId", (req: Request, res: Response) => {
+  console.log(req.params);
+  res.json("got single user");
+});
+
+// create a new user
+app.post("/api/users", (req: Request, res: Response) => {
   console.log(req.body);
-  res.json({ message: "received successfully" });
+  res.json({ message: "created successfully" });
 });
 
 export default app;
